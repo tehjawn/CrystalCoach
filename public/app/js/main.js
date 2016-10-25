c8e052fc076db0f21effbab87b70e6e43ca6c3c4
Xiaoshanna <tuf48938@temple.edu>
2016-10-24 23:56:21 -0400

    Messed around with ResponsiveVoiceJS and Web Speech API

--- public/app/js/main.js
+++ public/app/js/main.js
@@ -1,13 +1,41 @@
 var crystalApp = angular.module('crystalApp', [])
 
-crystalApp.controller('MainCtrl', function MainCtrl($scope) {
-	$scope.response = function () {
-		responsiveVoice.speak("Chet you scrub")
-	}
+crystalApp.controller('responseCtrl', ['$scope'] function responseCtrl($scope) {
+    $scope.response = function() {
+        responsiveVoice.speak("Chet you scrub")
+    }
 })
 
-crystalApp.controller('MainCtrl', function MainCtrl($scope) {
-	$scope.speech = function () {
-		recognizer.start();
-	}
+crystalApp.controller('speechCtrl', ['$scope'] function speechCtrl($scope) {
+    $scope.speech = function() {
+        var rec = new webkitSpeechRecognition();
+        var interim = [];
+        var final = '';
+
+        rec.continuous = false;
+        rec.lang = 'en-US';
+        rec.interimResults = true;
+        rec.onerror = function(event) {
+            console.log('error!');
+        };
+
+        start = function() {
+            rec.start();
+        };
+
+        rec.onresult = function(event) {
+            for (var i = event.resultIndex; i < event.results.length; i++) {
+                if (event.results[i].isFinal) {
+                    final = final.concat(event.results[i][0].transcript);
+                    console.log(event.results[i][0].transcript);
+                    $scope.$apply();
+                } else {
+                    interim.push(event.results[i][0].transcript);
+                    console.log('interim ' + event.results[i][0].transcript);
+                    $scope.$apply();
+                }
+            }
+        }
+    }
+
 })
